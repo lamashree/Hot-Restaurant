929658b180aef22da15c9f682068eb75e7b35a69
admin <admin@admins-MacBook-Pro.local>
2019-11-16 11:25:42 -0800

    added new changes

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var characters = [{
+    Name: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  }, {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  }, {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }];
+  
+  // Routes
+  // ===========================================================
+  
+  
